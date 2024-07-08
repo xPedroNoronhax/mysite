ab379e1bbd39cd7928b6e50a030c0e80d79ea15f
Pedro Noronha <pedro_noronha@outlook.com>
2024-07-08 14:21:43 -0300

    finalizando modulo de django templates

--- mysite/mysite/blog/admin.py
+++ mysite/mysite/blog/admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
-<<<<<<< HEAD
 from .models import Post
 admin.site.register(Post)
-=======
-from .models import Post
->>>>>>> f693bb18db0999667af0f0278d6d84fb4234d6ce
